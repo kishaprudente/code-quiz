70ef214d719670b3a7719024512801687a10a514
Kisha Prudente <kisha.prudente@gmail.com>
2020-04-04 19:19:56 -0400

    added questions array

---
+++ script.js
@@ -0,0 +1,42 @@
+var questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["A. <Javascript>", "B. <js>", "C. <script>"],
+    answer: 2,
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    choices: [
+      "A. The <head> section",
+      "B. The <body> section",
+      "C. Both are correct",
+    ],
+    answer: 1,
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choices: [
+      "A. <script href='xx.js'>",
+      "B. <script name='xx.js'>",
+      "C. <script src='xx.js'>",
+    ],
+    answer: 2,
+  },
+  {
+    question: "Which class provides a responsive fixed width container?",
+    choices: ["A. .container-fluid", "B. .container-fixed", "C. .container"],
+    answer: 1,
+  },
+  {
+    question: "What does HTML stand for?",
+    choices: [
+      "A. Hyper Text Markup Language",
+      "B. Hyperlinks and Text Markup Language",
+      "C. Home Tool Markup Language",
+    ],
+    answer: 0,
+  },
+];
+
+questions.forEach((question) => console.log(question));
